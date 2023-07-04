d78697b2b1854f12a63b060c68faab600188628f
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-07-04 18:46:35 -0500

    act ach

--- src/controllers/apiControllers.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-async function test(request: Request, response: Response): Promise<void> {
-  response.send('test Ok');
-}
-
-export default {
-  test
-};
--- src/routers/apiRouters.ts
+++
@@ -1,8 +0,0 @@
-const expresss = require('express');
-const apiControllers = require('../controllers/apiControllers');
-const router = expresss.Router();
-
-router
-  .post('/test', apiControllers.Test);
-
-module.exports = router;
