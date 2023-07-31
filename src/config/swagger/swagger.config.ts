e672d64b57e91da03342e3fcb5e8e28d57224031
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-07-30 22:12:53 -0500

    act

--- src/config/swagger/swagger.config.ts
+++ src/config/swagger/swagger.config.ts
@@ -1,7 +1,6 @@
 const swaggerUI = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const projectInfo = require('../../../package.json');
-const icon = require('../../assets/img/logo.png');
 
 const swaggerConfig = {
   failOnErrors: true,
@@ -71,7 +70,7 @@ const swaggerOptions = {
   filter: true,
   deepLinking: true,
   customSiteTitle: 'Api Rest Full Dynamic',
-  customFavIcon: {icon: icon},
+  customFavIcon: 'dist/asset/img/logo.png',
   customCss: ' .swagger-ui .topbar {display: none;}',
   customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css'
 };
