ee76bcb53ca6218ad6798327fb069ce883afd151
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-07-30 21:39:12 -0500

    act

--- src/config/swagger/swagger.config.ts
+++ src/config/swagger/swagger.config.ts
@@ -70,7 +70,7 @@ const swaggerOptions = {
     filter: true,
     deepLinking: true,
     customSiteTitle: 'Api Rest Full Dynamic',
-    customfavIcon: './src/asset/ico/favicon.ico',
+    customfavIcon: './dist/asset/ico/favicon.ico',
     customCss: ' .swagger-ui .topbar {display: none;}',
     customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css',
     customJsUrl: [
