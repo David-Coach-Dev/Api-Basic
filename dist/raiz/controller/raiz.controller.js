946e06f178f5deddce0b59d80a45150271436f08
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-08-02 12:48:14 -0500

    act

--- dist/raiz/controller/raiz.controller.js
+++ dist/raiz/controller/raiz.controller.js
@@ -1,10 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RaizController = void 0;
+const os_1 = __importDefault(require("os"));
 class RaizController {
     getRaiz(req, res) {
+        const user = os_1.default.userInfo();
+        console.log(user);
         res.status(200).json({
-            user: "Bienvenido a la Api Rest Full Dynamic."
+            user: `Bienvenido ${user.username} a la Api Rest Full Dynamic.`
         });
     }
 }
--- src/raiz/controller/raiz.controller.ts
+++ src/raiz/controller/raiz.controller.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
+import os from "os";
 
 export class RaizController {
   getRaiz(req: Request, res: Response) {
+    const user = os.userInfo();
+    console.log(user);
     res.status(200).json({
-      user: "Bienvenido a la Api Rest Full Dynamic."
+      user: `Bienvenido ${user.username} a la Api Rest Full Dynamic.`
     });
   }
 
