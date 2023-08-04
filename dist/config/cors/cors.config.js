16b23d56587e769520bc272b86d6141d8542391e
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-08-04 01:36:36 -0500

    act

--- dist/config/cors/cors.config.js
+++ dist/config/cors/cors.config.js
@@ -4,16 +4,18 @@ exports.corsConfig = void 0;
 const whitelist = ['http://localhost:8000', 'https://api-basic.vercel.app', 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css'];
 exports.corsConfig = {
     origin: (origin, callback) => {
-        if (!origin || whitelist.indexOf(origin) !== -1 || whitelist.some((allowedOrigin) => origin.startsWith(allowedOrigin))) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
             callback(null, true);
         }
         else {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-    optionsSuccessStatus: 200,
-    credentials: true,
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'HEAD', 'OPTIONS'],
+    optionsSuccessStatus: 204,
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON', 'Text/Plain', 'Text/HTML', 'application/x-www-form-urlencoded', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'Data', 'X-Api-Version', 'X-File-Name', 'charset=utf-8'],
+    exposedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON', 'Text/Plain', 'Text/HTML', 'application/x-www-form-urlencoded', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'Data', 'X-Api-Version', 'X-File-Name', 'charset=utf-8'],
+    maxAge: 63072000,
+    preflightContinue: false,
 };
 //# sourceMappingURL=cors.config.js.map
--- src/config/cors/cors.config.ts
+++ src/config/cors/cors.config.ts
@@ -1,17 +1,20 @@
 import { CorsOptions } from 'cors';
+import { Column } from 'typeorm';
 
 const whitelist = ['http://localhost:8000', 'https://api-basic.vercel.app','https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css'];
 
 export const corsConfig: CorsOptions = {
   origin: (origin: string | undefined, callback) => {
-    if (!origin || whitelist.indexOf(origin) !== -1 || whitelist.some((allowedOrigin) => origin.startsWith(allowedOrigin))) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON', 'Text/Plain', 'Text/HTML','application/x-www-form-urlencoded','X-CSRF-Token','Accept-Version', 'Content-Length', 'Content-MD5',  'Date','Data', 'X-Api-Version', 'X-File-Name','charset=utf-8'],
+  methods: ['GET', 'PUT', 'POST', 'DELETE','HEAD', 'OPTIONS'],
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON', 'Text/Plain', 'Text/HTML', 'application/x-www-form-urlencoded', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'Data', 'X-Api-Version', 'X-File-Name', 'charset=utf-8'],
+  exposedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON', 'Text/Plain', 'Text/HTML', 'application/x-www-form-urlencoded', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'Data', 'X-Api-Version', 'X-File-Name', 'charset=utf-8'],
+  maxAge: 63072000,
+  preflightContinue: false,
 };
