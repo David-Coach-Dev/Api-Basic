9713f0493298ac5041403f47397fc74f7c90960d
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-08-04 12:32:19 -0500

    act

--- dist/config/cors/cors.config.js
+++ dist/config/cors/cors.config.js
@@ -1,7 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsConfig = void 0;
-const whitelist = ['http://localhost:8000', 'https://api-basic.vercel.app'];
+const whitelist = [
+    'http://localhost:8000',
+    'https://api-basic.vercel.app',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css'
+];
 exports.corsConfig = {
     origin: (origin, callback) => {
         if (!origin || whitelist.indexOf(origin) !== -1) {
@@ -13,8 +17,8 @@ exports.corsConfig = {
     },
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'HEAD', 'OPTIONS'],
     optionsSuccessStatus: 204,
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON', 'Text/Plain', 'text/css', 'text/html', 'Text/HTML', 'application/x-www-form-urlencoded', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'Data', 'X-Api-Version', 'X-File-Name', 'charset=utf-8'],
-    exposedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON', 'Text/Plain', 'text/css', 'text/html', 'Text/HTML', 'application/x-www-form-urlencoded', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'Data', 'X-Api-Version', 'X-File-Name', 'charset=utf-8'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON', 'Text/Plain', 'text/javascript', 'text/css', 'text/html', 'Text/HTML', 'application/x-www-form-urlencoded', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'Data', 'X-Api-Version', 'X-File-Name', 'charset=utf-8'],
+    exposedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON', 'text/javascript', 'Text/Plain', 'text/css', 'text/html', 'Text/HTML', 'application/x-www-form-urlencoded', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'Data', 'X-Api-Version', 'X-File-Name', 'charset=utf-8'],
     maxAge: 63072000,
     preflightContinue: false,
 };
--- src/config/cors/cors.config.ts
+++ src/config/cors/cors.config.ts
@@ -1,6 +1,10 @@
 import { CorsOptions } from 'cors';
 
-const whitelist = ['http://localhost:8000', 'https://api-basic.vercel.app'];
+const whitelist = [
+  'http://localhost:8000',
+  'https://api-basic.vercel.app',
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css'
+];
 
 export const corsConfig: CorsOptions = {
   origin: (origin: string | undefined, callback) => {
@@ -12,8 +16,8 @@ export const corsConfig: CorsOptions = {
   },
   methods: ['GET', 'PUT', 'POST', 'DELETE','HEAD', 'OPTIONS'],
   optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON', 'Text/Plain','text/css', 'text/html','Text/HTML', 'application/x-www-form-urlencoded', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'Data', 'X-Api-Version', 'X-File-Name', 'charset=utf-8'],
-  exposedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON', 'Text/Plain','text/css', 'text/html', 'Text/HTML', 'application/x-www-form-urlencoded', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'Data', 'X-Api-Version', 'X-File-Name', 'charset=utf-8'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON', 'Text/Plain','text/javascript', 'text/css', 'text/html','Text/HTML', 'application/x-www-form-urlencoded', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'Data', 'X-Api-Version', 'X-File-Name', 'charset=utf-8'],
+  exposedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON','text/javascript', 'Text/Plain','text/css', 'text/html', 'Text/HTML', 'application/x-www-form-urlencoded', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'Data', 'X-Api-Version', 'X-File-Name', 'charset=utf-8'],
   maxAge: 63072000,
   preflightContinue: false,
 };
