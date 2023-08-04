0fe3e199980bb3d9ce709e45fde2c65c49b825ea
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-08-04 12:28:37 -0500

    act

--- dist/config/cors/cors.config.js
+++ dist/config/cors/cors.config.js
@@ -13,8 +13,8 @@ exports.corsConfig = {
     },
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'HEAD', 'OPTIONS'],
     optionsSuccessStatus: 204,
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON', 'Text/Plain', 'text/html', 'Text/HTML', 'application/x-www-form-urlencoded', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'Data', 'X-Api-Version', 'X-File-Name', 'charset=utf-8'],
-    exposedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON', 'Text/Plain', 'text/html', 'Text/HTML', 'application/x-www-form-urlencoded', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'Data', 'X-Api-Version', 'X-File-Name', 'charset=utf-8'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON', 'Text/Plain', 'text/css', 'text/html', 'Text/HTML', 'application/x-www-form-urlencoded', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'Data', 'X-Api-Version', 'X-File-Name', 'charset=utf-8'],
+    exposedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept', 'Application/JSON', 'Text/Plain', 'text/css', 'text/html', 'Text/HTML', 'application/x-www-form-urlencoded', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'Data', 'X-Api-Version', 'X-File-Name', 'charset=utf-8'],
     maxAge: 63072000,
     preflightContinue: false,
 };
