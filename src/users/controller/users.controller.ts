5b660cc30cdc37f6b5e728c3445d9597c424e051
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-08-09 18:29:27 -0500

    act

--- src/users/controller/users.controller.ts
+++ src/users/controller/users.controller.ts
@@ -4,7 +4,7 @@ export class UsersController {
   getUsers(req: Request, res: Response) {
     res.status(200).json({
       uptime: `${process.uptime()}`,
-      data: { user: '👨‍💻 Dc Dev -> David Caoch Dev.' },
+      data: { user: '👤 Dc Dev -> David Caoch Dev.' },
       timestamp: `${Date.now()}`
     });
   }
